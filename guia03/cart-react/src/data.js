e3ef08e99c26c1c89401287c667a280612c72fb4
Migue <miguel.urrutia@hotmail.es>
2025-02-08 11:05:35 -0600

    cart react

---
+++ guia03/cart-react/src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://prodimage.images-bn.com/pimages/9798890981745_p1_v2_s1200x1200.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://http2.mlstatic.com/D_NQ_NP_828656-MLU78222287707_082024-O.webp',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Cuentos de Barro',
+        price: 30,
+        urlImage: 'https://m.media-amazon.com/images/I/41dZy8CP52L._SY445_SX342_.jpg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Tierra de Infancia',
+        price: 30,
+        urlImage: 'https://leolibri.net/image/cache/catalog/2024/febrero%2026%20ev/Lars-TierradeInfancia-927x1000.jpg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        title: 'Harry Potter Pack',
+        price: 390,
+        urlImage: 'https://i5.walmartimages.com/seo/Pack-Harry-Potter-La-serie-completa-Harry-Potter-Paperback-Boxed-Set-Books-1-7-Paperback-9788418173196_a67bc885-2d2c-472c-80cb-7405523336b6.302fa50aa64667bc6cbb2c67d63997da.jpeg',
+        quantity: 1
+    }
+];
